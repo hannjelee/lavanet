947ffd593e366484e83d0f336e25e292b49219aa
hannjelee <111621354+hannjelee@users.noreply.github.com>
2024-05-01 15:29:12 +0700

    Update axelar.sh

--- axelar.sh
+++ axelar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://tm.axelar.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://tm.axelar.lava.build/lava-referer-0f20aacb-f4ff-45bb-8329-89c55beaeb86/"
 
 OUTPUT_DIR="logs"
 
